c1dcb3af42d821ecb95775c912b69aca6d1d953c
Anna-1902 <1077311@ucn.dk>
2022-03-08 13:31:25 +0100

    export default all functions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,58 @@
-module.exports = function getCompanyByVat (search, country) {
-    const axios = require("axios");
+import axios from "axios";
 
-    return new Promise((resolve, reject) => {
-        axios.get('https://cvrapi.dk/api?search=' + search + '&country=' + country )
-            .then((response) => {
-                resolve(response)
-            })
-            .catch((error) => {reject(error)})
-    })
+export default{
+    async getCompanyBySearch (search, country) {
+        let searchResult = ''
+
+        try {
+            await axios.get('https://cvrapi.dk/api?search=' + search + '&country=' + country )
+                .then((response) => {
+                    return searchResult = response
+                })
+        } catch (err) {alert(err.message)}
+    },
+
+    async getCompanyByVat (vat, country) {
+        let vatSearchResult = ''
+
+        try {
+            await axios.get('https://cvrapi.dk/api?vat=' + vat + '&country=' + country )
+                .then((response) => {
+                    return vatSearchResult = response
+                })
+        } catch (err) {alert(err.message)}
+    },
+
+    async getCompanyByName (name, country) {
+        let nameSearchResult = ''
+
+        try {
+            await axios.get('https://cvrapi.dk/api?name=' + name + '&country=' + country )
+                .then((response) => {
+                    return nameSearchResult = response
+                })
+        } catch (err) {alert(err.message)}
+    },
+
+    async getCompanyByProdu (produ, country) {
+        let produSearchResult = ''
+
+        try {
+            await axios.get('https://cvrapi.dk/api?produ=' + produ + '&country=' + country )
+                .then((response) => {
+                    return produSearchResult = response
+                })
+        } catch (err) {alert(err.message)}
+    },
+
+    async getCompanyByPhone (vat, country) {
+        let phoneSearchResult = ''
+
+        try {
+            await axios.get('https://cvrapi.dk/api?phone=' + phone + '&country=' + country )
+                .then((response) => {
+                    return phoneSearchResult = response
+                })
+        } catch (err) {alert(err.message)}
+    }
 }
